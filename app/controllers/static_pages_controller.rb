8392a564339c62131c8fc713225270c1e0a0c87b
Yinka Ash <developer@designsbynetguru.com>
2017-05-19 10:26:21 +0800

    Added Index, Services & About Static Pages With Styles

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+
+	def index
+		
+	end
+
+	def services
+		
+	end
+
+	def about
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get 'posts', to: 'static_pages#index'
+  get 'weblog', to: 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  get 'services', to: 'static_pages#services'
+  root 'static_pages#index'
+
 end
