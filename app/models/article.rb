31353a5c6d4d99c88d17b011d265353d0c4ec65d
Yinka Ash <developer@designsbynetguru.com>
2017-05-19 17:11:41 +0800

    Fix Bugs

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
 	belongs_to :user
 	has_many :comments
+
+	validates :title, presence: true, length: { maximum: 30 }
+	validates :description, presence: true, length: { minimum: 10 }
 end
