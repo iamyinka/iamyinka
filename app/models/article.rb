0c42823315b8d2324dc8bb1c6db052de324c5448
Yinka Ash <developer@designsbynetguru.com>
2017-05-19 17:15:25 +0800

    Fix Title length

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
 	belongs_to :user
 	has_many :comments
 
-	validates :title, presence: true, length: { maximum: 30 }
+	validates :title, presence: true, length: { maximum: 50 }
 	validates :description, presence: true, length: { minimum: 10 }
 end
